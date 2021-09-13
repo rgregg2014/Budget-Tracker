c1b5c48ad597c0fcdf31645905558f2a0115ba83
Rachel Gregg <rgregg2014@gmail.com>
2021-09-13 18:53:29 -0400

    initialized caches for data and static assets

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,22 @@
-console.log("Inside the service worker file");
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/index.js",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "icons/icon-512x512.png",
+];
+
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+  );
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
