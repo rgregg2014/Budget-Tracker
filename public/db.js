dff8aa7df5a3e3269e656021d77d32d508c39acd
Rachel Gregg <rgregg2014@gmail.com>
2021-09-13 19:27:21 -0400

    added IndexedDB functionality

--- public/db.js
+++ public/db.js
@@ -1,3 +1,62 @@
-//CREATE A NEW INDEXEDDB INSTANCE FOR OFFLINE USE
+//Create a new IDB database for online use
 let db;
-const request = indexedDB.open("transaction", 1);
+const request = indexedDB.open("budget", 1);
+
+//Create a new Object Store
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createdObjectStore("pending", { autoIncrement: true });
+};
+
+//Check if navigator is online. If it is check the database
+request.onsuccess = function (event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+//Catch for an error
+request.onerror = function (event) {
+  console.log("Something went wrong!" + event.target.errorCode);
+};
+
+/*
+=========== HELPER FUNCTIONS
+*/
+
+//Create a new transaction in the budget database, pending Object Store
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+}
+
+//When the app is online, take anything from the IDB and POST to Mongo Database
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("api/trasnaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text,plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
+
+//Listen for window to go back online, run checkDatabase function
+window.addEventListener("online", checkDatabase);
